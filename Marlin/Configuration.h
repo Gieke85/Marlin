b36ac68988bb064e9f2d4d11b50fea3563a6f214
Gieke85 <gieke85@gmail.com>
2020-10-14 19:34:09 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1105,7 +1105,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250
+#define Z_MAX_POS 225         //Default: 250
 
 /**
  * Software Endstops
